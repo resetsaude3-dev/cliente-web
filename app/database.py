0caf48aed3ec3654b28c31cb47097410f12464c1
hd <resetsaude3@gmail.com>
2026-04-05 18:07:50 -0300

    conecta postgres render

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./clientes.db"
+# pega do Render (PostgreSQL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# fallback (caso rode local)
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./clientes.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
 
 SessionLocal = sessionmaker(
